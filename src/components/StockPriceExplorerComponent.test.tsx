7b95be68afca5926fe1bca3a2bdc1a318527e266
James Roseman <james.roseman@gmail.com>
2022-10-04 14:41:38 +0100

    StockPriceExplorer props defined and created. The component is rendering in the main app.

--- src/components/StockPriceExplorerComponent.test.tsx
+++ src/components/StockPriceExplorerComponent.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import StockPriceExplorer from './StockPriceExplorerComponent';
 
 test('renders init message', () => {
-  render(<StockPriceExplorer />);
+  render(<StockPriceExplorer dates={[]} prices={{}} />);
   const helloWorldElement = screen.getByText(/Hello, world!/i);
   expect(helloWorldElement).toBeInTheDocument();
 })
--- src/components/StockPriceExplorerComponent.tsx
+++ src/components/StockPriceExplorerComponent.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import './StockPriceExplorer.css';
 
-function StockPriceExplorer() {
+/**
+ * A mapping from stock price tickers to an array of 
+ * prices.
+ * 
+ * e.g. AAPL: [19.72, 18.58, null, 31.38]
+ */
+export type StockPricesMap = {
+  [ticker: string]: (number|null)[];
+}
+
+export type StockPriceExplorerProps = {
+  dates: string[],
+  prices: StockPricesMap,
+}
+
+function StockPriceExplorer({
+  dates,
+  prices
+}: StockPriceExplorerProps) {
   return (
-    <div>
+    <div className="container">
       Hello, world!
     </div>
   )
